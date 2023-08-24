c98a429215f061c69d9c25b344b290ffb1d56a20
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-08-24 09:11:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,11 +11,6 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
-const cartList = document.getElementById("cart-list");
-const clearCartBtn = document.getElementById("clear-cart-btn");
-
-// Retrieve cart data from session storage
-let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
 // Render product list
 function renderProducts() {
@@ -85,3 +80,4 @@ cartList.addEventListener("click", (event) => {
 
 // Add event listener to clear-cart button
 clearCartBtn.addEventListener("click", clearCart);
+
